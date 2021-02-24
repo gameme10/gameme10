cfba0bf36589241b9a03c64192dfbd324ca673d7
gameme10 <66317738+gameme10@users.noreply.github.com>
2021-02-24 17:58:55 +0100

    Update GetURLparam.js

--- GetURLparam.js
+++ GetURLparam.js
@@ -4,3 +4,15 @@
     results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+  function pagosDontHack()
+{
+    // Cogemos los valores pasados por get
+    //recogemos los valores que nos envia la URL en variables para trabajar con ellas
+    var skuURL = getParameterByName('skuURL');
+    var ver = getParameterByName('ver');
+    if(skuURL && ver == "aRS9rD*rg%V5%BF4@@isyJiJif@T51AABgBLl0J0dNsmo7lJ6"){document.getElementById("pagar").value = skuURL}else{
+        // no se ha recibido ningun parametro por GET
+        document.getElementById("pagar").innerHTML = '';
+    }
+}
